2a33d1e5d665137902ead4f06a02c6cfe78f021e
Sajid Alam <official.sajid669@gmail.com>
2022-11-22 20:55:15 +0530

    all changes

--- src/components/pages/user/AddUser.js
+++ src/components/pages/user/AddUser.js
@@ -17,14 +17,20 @@ function AddUser() {
 
   function Submit(e) {
     e.preventDefault();
-    axios.post("http://localhost:3333/contacts", data).then(navigate("/"));
+    console.log({ data });
+    axios.post("http://localhost:3333/contacts", data).then(() => {
+      navigate("/");
+    });
   }
   return (
     <div className="w-screen h-ful flex flex-col justify-center items-center mt-16">
       <h1 className="text-black text-3xl font-semibold font-monterse">
         Add User
       </h1>
-      <form className="w-[80%] h-full flex flex-col justify-center items-center mt-4">
+      <form
+        onSubmit={Submit}
+        className="w-[80%] h-full flex flex-col justify-center items-center mt-4"
+      >
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
@@ -47,7 +53,7 @@ function AddUser() {
           className="w-[80%] bg-white text-xl font-Montserrat font-normal outline-none mt-4 py-4 pl-6 border bord-zinc-400"
         />
         <button
-          onSubmit={Submit}
+          type="submit"
           className="w-[80%] bg-blue-600  text-white text-xl font-Montserrat font-semibold mt-4 py-4 pl-6 "
         >
           Add User
